f4780434f774fd18641dc028b7b474366bbaa000
kolehmaineniina <kolehmaineniina@gmail.com>
2025-08-24 23:44:09 +0300

    second commit

--- App.js
+++ App.js
@@ -1,16 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [guess, setGuess] = useState('');
+  const [text, setText] = useState('Guess a number between 1 and 100');
+  const [count, setCount] = useState(0);
+  const [won, setWon] = useState(false);
+
+  const [numberToGuess, setNumberToGuess] = useState(Math.floor(Math.random() * 100) + 1);
+  
+  const submitGuess = () => {
+    if (won) {
+      return;
+    }
+    
+    const n = parseInt(guess);
+
+    if (isNaN(n) || n < 1 || n > 100) {
+      setText('Please enter a valid number between 1 and 100.');
+      return;
+    }
+    setCount(count + 1);
+    if (n === numberToGuess) {
+      setText(`Congratulations! You guessed the number in ${count} tries`);
+      setWon(true);
+    } else if (n < numberToGuess) {
+      setText(`Your guess ${guess} is too low.`);
+    } else {
+      setText(`Your guess ${guess} is too high.`);
+    }
+  };
+
+  const resetGame = () => {
+    setGuess('');
+    setText('Guess a number between 1 and 100');
+    setCount(0);
+    setWon(false);
+    setNumberToGuess(Math.floor(Math.random() * 100) + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Guess the number!</Text>
-      <Text>Your guess: {guess}</Text>
+      <Text style={styles.title}>Guess the number!</Text>
+      <Text>{text}</Text>
       <TextInput 
-      placeholder="Enter a number between 1 and 100"
-      keyboardType="numeric"
+        placeholder="Enter your guess"
+        keyboardType="numeric"
+        value={guess}
+        onChangeText={(guess) => setGuess(guess)}
+        style={styles.input}
       />
-      <Button title="Make a guess" onPress={() => {}} />
+      <View style={styles.buttonContainer}>
+        <Button title="Make a guess"
+          onPress={submitGuess}
+          disabled={!guess.trim()}
+        />
+        <Button title="Play again" onPress={resetGame} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,5 +69,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 12,
+    paddingLeft: 8,
+    width: '50%',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    justifyContent: 'space-evenly',
+    height: 100,
   },
 });
